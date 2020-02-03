f88875ee2d7e5db78cb1561a2b6674e11ca9a882
Alex Contreras <ac@jexan.com>
2020-02-02 23:30:40 -0600

    Start redux setup

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:30" },
+    { title: "All Star", duration: "3:15" },
+    { title: "Nothing Else Matters", duration: "6:25" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
